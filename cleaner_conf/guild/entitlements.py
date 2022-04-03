6f39b7977b0b95d3889a2507c3b5bce8d58301cd
Leo Developer <leodeveloper@protonmail.com>
2022-04-03 02:05:03 +0200

    revert

--- cleaner_conf/guild/entitlements.py
+++ cleaner_conf/guild/entitlements.py
@@ -4,25 +4,23 @@ Entitlments are read-only.
 
 from pydantic import BaseModel, conint
 
-conplan = conint(ge=0, le=2)
-
 
 class GuildEntitlements(BaseModel):
-    plan: conplan = 0
+    plan: conint(ge=0, le=2) = 0
     suspended: bool = False
     partnered: bool = False
-    workers: conplan = 1
+    workers: conint(ge=0, le=2) = 1
     workers_cpu: conint(ge=10, le=50) = 10
     workers_ram: conint(ge=1024 * 128, le=1024 * 1024) = 1024 * 128
     workers_size: conint(ge=1024 * 16, le=1024 * 256) = 1024 * 256
-    challenge_interactive_join_risk: conplan = 1
-    challenge_interactive_custom_embed: conplan = 1
-    challenge_interactive_custom_webpage: conplan = 1
-    contact_standard: conplan = 1
-    contact_email: conplan = 1
+    challenge_interactive_join_risk: conint(ge=0, le=2) = 1
+    challenge_interactive_custom_embed: conint(ge=0, le=2) = 1
+    challenge_interactive_custom_webpage: conint(ge=0, le=2) = 1
+    contact_standard: conint(ge=0, le=2) = 1
+    contact_email: conint(ge=0, le=2) = 1
     bot_limit: conint(ge=0, le=100) = 0
-    bot_dedicated: conplan = 2
-    logging_downloads: conplan = 1
+    bot_dedicated: conint(ge=0, le=2) = 2
+    logging_downloads: conint(ge=0, le=2) = 1
     logging_retention: conint(ge=3, le=12) = 3
-    impersonation_advanced: conplan = 1
-    verification_custom_webpage: conplan = 1
+    impersonation_advanced: conint(ge=0, le=2) = 1
+    verification_custom_webpage: conint(ge=0, le=2) = 1
