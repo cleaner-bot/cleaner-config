f9d950b87cea0e8e9819ab2678c6e4d44d0dcf27
Leo Developer <leodeveloper@protonmail.com>
2022-04-13 23:39:24 +0200

    rename a few entitlements

--- cleaner_conf/guild/entitlements.py
+++ cleaner_conf/guild/entitlements.py
@@ -14,8 +14,6 @@ class GuildEntitlements(BaseModel):
     workers_ram: conint(ge=1024 * 128, le=1024 * 1024) = 1024 * 128
     workers_size: conint(ge=1024 * 16, le=1024 * 256) = 1024 * 256
     challenge_interactive_join_risk: conint(ge=0, le=2) = 1
-    challenge_interactive_custom_embed: conint(ge=0, le=2) = 1
-    challenge_interactive_custom_webpage: conint(ge=0, le=2) = 1
     contact_standard: conint(ge=0, le=2) = 1
     contact_email: conint(ge=0, le=2) = 1
     bot_limit: conint(ge=0, le=100) = 0
@@ -23,4 +21,5 @@ class GuildEntitlements(BaseModel):
     logging_downloads: conint(ge=0, le=2) = 1
     logging_retention: conint(ge=3, le=12) = 3
     impersonation_advanced: conint(ge=0, le=2) = 1
-    verification_custom_webpage: conint(ge=0, le=2) = 1
+    branding_splash: conint(ge=0, le=2) = 1
+    branding_embed: conint(ge=0, le=2) = 1
