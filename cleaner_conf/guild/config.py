7fec2d975a1e54b01084079ae30a360555019140
Leo Developer <leodeveloper@protonmail.com>
2022-04-14 13:28:09 +0200

    update snowflake regex

--- cleaner_conf/guild/config.py
+++ cleaner_conf/guild/config.py
@@ -3,6 +3,8 @@ Config variables are read/write.
 """
 from pydantic import BaseModel, Field, conint, conlist, constr
 
+snowflake_re = r"^(0|\d{17,21})$"
+
 
 class GuildConfig(BaseModel):
     rules_phishing_content: conint(ge=0, le=2) = 2
@@ -25,29 +27,29 @@ class GuildConfig(BaseModel):
     antiraid_enabled: bool = False
     antiraid_limit: constr(regex=r"^\d{1,5}/\d{1,3}$") = "10/10"
     general_modroles: conlist(
-        constr(regex=r"^\d{1,21}$"), max_items=256, unique_items=True
+        constr(regex=snowflake_re), max_items=256, unique_items=True
     ) = Field(default_factory=list)
     general_dehoisting_enabled: bool = True
     slowmode_enabled: bool = True
     slowmode_exceptions: conlist(
-        constr(regex=r"^\d{1,21}$"), max_items=256, unique_items=True
+        constr(regex=snowflake_re), max_items=256, unique_items=True
     ) = Field(default_factory=list)
     challenge_timeout_enabled: bool = True
     challenge_interactive_enabled: bool = False
     challenge_interactive_take_role: bool = False
-    challenge_interactive_role: constr(regex=r"^\d{1,21}$") = "0"
+    challenge_interactive_role: constr(regex=snowflake_re) = "0"
     challenge_interactive_joinrisk_custom: conint(ge=0, le=100) = 70
     challenge_interactive_level: conint(ge=0, le=5) = 2
     challenge_interactive_webpage_splash: constr(
         regex="^$|^https?://.+", max_length=256
     ) = ""
     verification_enabled: bool = False
-    verification_role: constr(regex=r"^\d{1,21}$") = "0"
+    verification_role: constr(regex=snowflake_re) = "0"
     verification_webpage_splash: constr(
         regex="^$|^https?://.+", max_length=256
     ) = ""
     logging_enabled: bool = False
-    logging_channel: constr(regex=r"^\d{1,21}$") = "0"
+    logging_channel: constr(regex=snowflake_re) = "0"
     logging_option_join: bool = False
     logging_downloads_enabled: bool = False
     impersonation_discord_enabled: bool = True
