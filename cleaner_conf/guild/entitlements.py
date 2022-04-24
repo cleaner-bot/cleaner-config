b2beaa7ba5aba3c428cd7e296dc1c8d78910a27b
Leo Developer <leodeveloper@protonmail.com>
2022-04-24 20:39:17 +0200

    refactor a lot of entitlements

--- cleaner_conf/guild/entitlements.py
+++ cleaner_conf/guild/entitlements.py
@@ -9,19 +9,28 @@ class GuildEntitlements(BaseModel):
     plan: conint(ge=0, le=2) = 0
     suspended: bool = False
     partnered: bool = False
-    workers: conint(ge=0, le=2) = 1
-    workers_cpu: conint(ge=10, le=50) = 10
-    workers_ram: conint(ge=1024 * 128, le=1024 * 1024) = 1024 * 128
-    workers_size: conint(ge=1024 * 16, le=1024 * 256) = 1024 * 256
+    
+    statistics: conint(ge=0, le=2) = 1
+    antiraid: conint(ge=0, le=2) = 0  # unused
+    antispam: conint(ge=0, le=2) = 0  # unused
+    backup: conint(ge=0, le=2) = 1
+    bot_limit: conint(ge=0, le=100) = 0  # unused
+    bot_dedicated: conint(ge=0, le=2) = 2  # unused
+    branding_splash: conint(ge=0, le=2) = 1
+    branding_embed: conint(ge=0, le=2) = 1  # unused
+    branding_vanity: conint(ge=0, le=2) = 1  # unused
     challenge_interactive_join_risk: conint(ge=0, le=2) = 1
     contact_standard: conint(ge=0, le=2) = 1
     contact_email: conint(ge=0, le=2) = 1
-    bot_limit: conint(ge=0, le=100) = 0
-    bot_dedicated: conint(ge=0, le=2) = 2
+    firewall: conint(ge=0, le=2) = 0  # unused
+    impersonation: conint(ge=0, le=2) = 0  # unused
+    impersonation_advanced: conint(ge=0, le=2) = 1
+    logging: conint(ge=0, le=2) = 0  # unused
     logging_downloads: conint(ge=0, le=2) = 1
     logging_retention: conint(ge=3, le=12) = 3
-    impersonation_advanced: conint(ge=0, le=2) = 1
-    analytics: conint(ge=0, le=2) = 1
-    branding_splash: conint(ge=0, le=2) = 1
-    branding_embed: conint(ge=0, le=2) = 1
-    branding_vanity: conint(ge=0, le=2) = 1
+    slowmode: conint(ge=0, le=2) = 0  # unused
+    verification: conint(ge=0, le=2) = 0  # unused
+    workers: conint(ge=0, le=2) = 1  # unused
+    workers_cpu: conint(ge=10, le=50) = 10  # unused
+    workers_ram: conint(ge=1024 * 128, le=1024 * 1024) = 1024 * 128  # unused
+    workers_size: conint(ge=1024 * 16, le=1024 * 256) = 1024 * 256  # unused
