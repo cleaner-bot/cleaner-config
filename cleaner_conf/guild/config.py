c2107556faf93ea7340b953f264576df81e8b613
Leo Developer <leodeveloper@protonmail.com>
2022-05-10 20:17:11 +0200

    add antiraid_metadata

--- cleaner_conf/guild/config.py
+++ cleaner_conf/guild/config.py
@@ -26,6 +26,7 @@ class GuildConfig(BaseModel):
     antispam_attachment: bool = True
     antiraid_enabled: bool = False
     antiraid_limit: constr(regex=r"^\d{1,5}/\d{1,3}$") = "10/10"
+    antiraid_metadata: bool = False
     general_modroles: conlist(
         constr(regex=snowflake_re), max_items=256, unique_items=True
     ) = Field(default_factory=list)
