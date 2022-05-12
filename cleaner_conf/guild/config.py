c643ac017804f5db7296162bff933e4c6e584044
Leo Developer <leodeveloper@protonmail.com>
2022-05-12 17:27:46 +0200

    fix issue

--- cleaner_conf/guild/config.py
+++ cleaner_conf/guild/config.py
@@ -9,71 +9,71 @@ snowflake_re = r"^(0|\d{17,21})$"
 class GuildConfig(BaseModel):
     rules_phishing_content: conint(ge=0, le=2) = 2
     rules_phishing_content_channels: conlist(
-        constr(regex=snowflake_re, max_items=25, unique_items=True)
+        constr(regex=snowflake_re), max_items=25, unique_items=True
     ) = Field(default_factory=list)
     rules_phishing_domain_blacklisted: conint(ge=0, le=2) = 2
     rules_phishing_domain_blacklisted_channels: conlist(
-        constr(regex=snowflake_re, max_items=25, unique_items=True)
+        constr(regex=snowflake_re), max_items=25, unique_items=True
     ) = Field(default_factory=list)
     rules_phishing_domain_heuristic: conint(ge=0, le=2) = 2
     rules_phishing_domain_heuristic_channels: conlist(
-        constr(regex=snowflake_re, max_items=25, unique_items=True)
+        constr(regex=snowflake_re), max_items=25, unique_items=True
     ) = Field(default_factory=list)
     rules_phishing_embed: conint(ge=0, le=2) = 2
     rules_phishing_embed_channels: conlist(
-        constr(regex=snowflake_re, max_items=25, unique_items=True)
+        constr(regex=snowflake_re), max_items=25, unique_items=True
     ) = Field(default_factory=list)
     rules_selfbot_embed: conint(ge=0, le=2) = 2
     rules_selfbot_embed_channels: conlist(
-        constr(regex=snowflake_re, max_items=25, unique_items=True)
+        constr(regex=snowflake_re), max_items=25, unique_items=True
     ) = Field(default_factory=list)
     rules_ping_hidden: conint(ge=0, le=2) = 2
     rules_ping_hidden_channels: conlist(
-        constr(regex=snowflake_re, max_items=25, unique_items=True)
+        constr(regex=snowflake_re), max_items=25, unique_items=True
     ) = Field(default_factory=list)
     rules_ping_roles: conint(ge=0, le=2) = 2
     rules_ping_roles_channels: conlist(
-        constr(regex=snowflake_re, max_items=25, unique_items=True)
+        constr(regex=snowflake_re), max_items=25, unique_items=True
     ) = Field(default_factory=list)
     rules_ping_users_many: conint(ge=0, le=2) = 2
     rules_ping_users_many_channels: conlist(
-        constr(regex=snowflake_re, max_items=25, unique_items=True)
+        constr(regex=snowflake_re), max_items=25, unique_items=True
     ) = Field(default_factory=list)
     rules_ping_users_few: conint(ge=0, le=2) = 1
     rules_ping_users_few_channels: conlist(
-        constr(regex=snowflake_re, max_items=25, unique_items=True)
+        constr(regex=snowflake_re), max_items=25, unique_items=True
     ) = Field(default_factory=list)
     rules_ping_broad: conint(ge=0, le=2) = 1
     rules_ping_broad_channels: conlist(
-        constr(regex=snowflake_re, max_items=25, unique_items=True)
+        constr(regex=snowflake_re), max_items=25, unique_items=True
     ) = Field(default_factory=list)
     rules_advertisement_discord_invite: conint(ge=0, le=2) = 1
     rules_advertisement_discord_invite_channels: conlist(
-        constr(regex=snowflake_re, max_items=25, unique_items=True)
+        constr(regex=snowflake_re), max_items=25, unique_items=True
     ) = Field(default_factory=list)
     rules_emoji_mass: conint(ge=0, le=2) = 0
     rules_emoji_mass_channels: conlist(
-        constr(regex=snowflake_re, max_items=25, unique_items=True)
+        constr(regex=snowflake_re), max_items=25, unique_items=True
     ) = Field(default_factory=list)
     antispam_similar: bool = True
     antispam_similar_channels: conlist(
-        constr(regex=snowflake_re, max_items=25, unique_items=True)
+        constr(regex=snowflake_re), max_items=25, unique_items=True
     ) = Field(default_factory=list)
     antispam_exact: bool = True
     antispam_exact_channels: conlist(
-        constr(regex=snowflake_re, max_items=25, unique_items=True)
+        constr(regex=snowflake_re), max_items=25, unique_items=True
     ) = Field(default_factory=list)
     antispam_token: bool = True
     antispam_token_channels: conlist(
-        constr(regex=snowflake_re, max_items=25, unique_items=True)
+        constr(regex=snowflake_re), max_items=25, unique_items=True
     ) = Field(default_factory=list)
     antispam_sticker: bool = True
     antispam_sticker_channels: conlist(
-        constr(regex=snowflake_re, max_items=25, unique_items=True)
+        constr(regex=snowflake_re), max_items=25, unique_items=True
     ) = Field(default_factory=list)
     antispam_attachment: bool = True
     antispam_attachment_channels: conlist(
-        constr(regex=snowflake_re, max_items=25, unique_items=True)
+        constr(regex=snowflake_re), max_items=25, unique_items=True
     ) = Field(default_factory=list)
     antiraid_enabled: bool = False
     antiraid_limit: constr(regex=r"^\d{1,5}/\d{1,3}$") = "10/10"
